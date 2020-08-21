38367b9573ead8df3ad5d01bbeb490dc4bcca161
hzxGoForward <hzx@pku.edu.cn>
2020-08-21 11:17:51 +0800

    修改文件存储目录为0821, 增加了版本描述

--- src/hzxLog.h
+++ src/hzxLog.h
@@ -9,13 +9,15 @@
 using namespace std;
 static int64_t writeDiff = 120; // 120秒写一次
 static int64_t lastMempoolRecordTime = 0;
-static string dir = "/home2/zxhu/bitcoin-0.19.0_hzx/experiment20200820_3/";
+static string dir = "/home2/zxhu/bitcoin-0.19.0_hzx/experiment20200821/";
 /*
 1. 这一版本中，预测新交易时，取消了区块验证过程
 2. 在收到新的区块后，进行预测时增加权重100000
 3. 每次预测后，检查生成的交易是否在预测序列中，如果在其中才算成功。
 4. 每次预测一批交易的时候，记录这批交易加入区块时隶属的组合费率
 5. 文件保存位置为experiment20200820_3
+6. 1. 修复compare中，预测交易序列的交易大小没有加上空格的bug。
+7. lastFeerate用comparebyDecendentSocre的bug。
 */
 
 
